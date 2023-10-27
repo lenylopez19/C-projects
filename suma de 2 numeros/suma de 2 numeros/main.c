9ef750d24d6b5e0c649cb009ac9df9bd3df197db
lenylopez19 <leny.lopez19@gmail.com>
2023-10-27 01:50:16 -0400

    +

---
+++ suma de 2 numeros/suma de 2 numeros/main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  suma de 2 numeros
+//
+//  Created by leny Lopez on 12/10/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    int num1 ; int num2; int sum;
+    num1 = 10;
+    num2 = 10;
+    sum = num1 + num2;
+    printf("%d",sum);
+    
+}
